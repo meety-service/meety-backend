7d38e857ba373b066972bd35ee6bc6f86911667a
Hyemin Lee <cotloz88@gmail.com>
2023-11-18 14:47:46 +0900

    feat: implement skeleton of `meetings` module

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Patch,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 
 @Controller('meetings')
@@ -6,7 +14,33 @@ export class MeetingsController {
   constructor(private readonly meetingsService: MeetingsService) {}
 
   @Get()
-  getHelloWorld(): string {
-    return this.meetingsService.getHelloWorld();
+  getMeetings() {
+    return this.meetingsService.getMeetings();
+  }
+
+  @Delete('/:id')
+  deleteMeetingById(@Param('id') meetingId: number) {
+    const success = this.meetingsService.deleteMeetingById(meetingId);
+    return { success };
+  }
+
+  @Patch('/:id/hiding')
+  patchMeetingById(
+    @Param('id') meetingId: number,
+    @Body('list_visible') listVisible: number,
+  ) {
+    const success = this.meetingsService.patchMeetingById(meetingId, listVisible);
+    return {success};
+  }
+
+  @Post()
+  createMeeting() {
+    const success = this.meetingsService.createMeeting();
+    return {success};
+  }
+
+  @Get('/:id')
+  getMeetingById(@Param('id') meetingId: number) {
+    return this.meetingsService.getMeetingById(meetingId);
   }
 }
