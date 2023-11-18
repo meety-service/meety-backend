7ab62f9020c6ac25da4540dc383e69ecec79028d
Hyemin Lee <cotloz88@gmail.com>
2023-11-18 17:18:56 +0900

    implement http exception filter for each module (#12)

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MeetingsService } from './meetings.service';
 import { MeetingsController } from './meetings.controller';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Module({
   controllers: [MeetingsController],
-  providers: [MeetingsService],
+  providers: [
+    MeetingsService,
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class MeetingsModule {}
