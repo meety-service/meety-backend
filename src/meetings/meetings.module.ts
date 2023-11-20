c851d204f3518fbb235038a61c3d11bae0243270
Hyemin Lee <cotloz88@gmail.com>
2023-11-20 11:39:33 +0900

    fix: meeting module 데코레이터에서 엔티티 등록 추가

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -5,9 +5,21 @@ import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/common/exception-filter/http-exception.filter';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Meeting } from 'src/entity/meeting.entity';
+import { MeetingMember } from 'src/entity/meetingMember.entity';
+import { MeetingDate } from 'src/entity/meetingDate.entity';
+import { Member } from 'src/entity/member.entity';
+import { Timezone } from 'src/entity/timezone.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Meeting])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Meeting,
+      MeetingMember,
+      MeetingDate,
+      Member,
+      Timezone,
+    ]),
+  ],
   controllers: [MeetingsController],
   providers: [
     MeetingsService,
