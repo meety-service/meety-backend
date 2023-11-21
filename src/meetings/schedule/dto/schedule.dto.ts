ff0243c3a417528dce6d5e34d9fcdbad85684ea1
Hyemin Lee <cotloz88@gmail.com>
2023-11-21 22:32:24 +0900

    update schedule dto

--- src/meetings/schedule/dto/schedule.dto.ts
+++ src/meetings/schedule/dto/schedule.dto.ts
@@ -1,6 +1,11 @@
 import { MeetingDate } from "src/entity/meetingDate.entity";
 
+export class SelectTimetableDto {
+    date: string;
+    times: string[];
+}
+
 export class ScheduleDto {
     nickname: string;
-    selected_items: MeetingDate[]
+    selected_items: SelectTimetableDto[];
 }
