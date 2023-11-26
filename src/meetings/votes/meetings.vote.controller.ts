c2b0e55129b917f08e0a9e4b73d61625bbab23c6
teria314 <bacteria3141592@gmail.com>
2023-11-26 20:22:08 +0900

    투표 api에 유저식별 로직  예외처리 추가

--- src/meetings/votes/meetings.vote.controller.ts
+++ src/meetings/votes/meetings.vote.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Body, Put, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Put, Patch, Req } from '@nestjs/common';
 import { MeetingsVoteService } from './meetings.vote.service';
 import { CreateVoteDto, FetchVoteDto, VoteCloseDto } from './dto/vote.dto';
 import { UserChoiceDto } from './dto/vote.choice.dto';
@@ -8,28 +8,28 @@ export class MeetingsVoteController {
   constructor(private readonly meetingsVoteService: MeetingsVoteService) {}
   
   @Post()
-  createVote(@Param('id') meeting_id: number, @Body() createVoteDto: CreateVoteDto){
-    return this.meetingsVoteService.createVote(meeting_id, createVoteDto);
+  createVote(@Param('id') meeting_id: number, @Body() createVoteDto: CreateVoteDto, @Req() req : Request){
+    return this.meetingsVoteService.createVote(meeting_id, createVoteDto, req);
   }
 
   @Get()
-  fetchVote(@Param('id') meeting_id: number){
-    return this.meetingsVoteService.fetchVote(meeting_id);
+  fetchVote(@Param('id') meeting_id: number, @Req() req : Request){
+    return this.meetingsVoteService.fetchVote(meeting_id, req);
   }
 
   @Post('/choice')
-  createUserChoice(@Param('id') meeting_id: number, @Body() userChoiceDto: UserChoiceDto){
-    return this.meetingsVoteService.createUserChoice(meeting_id, userChoiceDto);
+  createUserChoice(@Param('id') meeting_id: number, @Body() userChoiceDto: UserChoiceDto, @Req() req : Request){
+    return this.meetingsVoteService.createUserChoice(meeting_id, userChoiceDto, req);
   }
 
   @Put('/choice')
-  updateUserChoice(@Param('id') meeting_id: number, @Body() userChoiceDto: UserChoiceDto){
-    return this.meetingsVoteService.updateUserChoice(meeting_id, userChoiceDto);
+  updateUserChoice(@Param('id') meeting_id: number, @Body() userChoiceDto: UserChoiceDto, @Req() req : Request){
+    return this.meetingsVoteService.updateUserChoice(meeting_id, userChoiceDto, req);
   }
 
   @Patch()
-  closeVote(@Param('id') meeting_id:number, @Body() voteCloseDto : VoteCloseDto){
-    return this.meetingsVoteService.closeVote(meeting_id,voteCloseDto);
+  closeVote(@Param('id') meeting_id:number, @Body() voteCloseDto : VoteCloseDto, @Req() req : Request){
+    return this.meetingsVoteService.closeVote(meeting_id,voteCloseDto, req);
   }
 
 }
