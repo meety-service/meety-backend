0e967e01dc85ba868d256d135b6e9dfe18df13d2
2019013436 <limbell@hanyang.ac.kr>
2023-12-01 18:53:37 +0900

    parsetoken 수정

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ export function parseToken(headers: Headers) {
   const cookieName = 'X-Gapi-Refresh-Token';
   const cookie = headers['cookie'] || headers['Cookie']; //쿠키로 클라이언트로부터 코드 전달받기
   if (!cookie || !cookie.includes(`${cookieName}=`)) {
-    return null;
+    return undefined;
   }
 
   return cookie
